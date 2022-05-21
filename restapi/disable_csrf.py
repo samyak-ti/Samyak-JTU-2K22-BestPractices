ef16c3dc3eb7612ccd88fb63970161a3e364082b
samyak-ti <samyak.samdariya@trilogy.com>
2022-05-21 23:19:32 +0530

    Renamed Classes according to naming conventions

--- restapi/disable_csrf.py
+++ restapi/disable_csrf.py
@@ -1,6 +1,6 @@
 from django.utils.deprecation import MiddlewareMixin
 
 
-class DisableCSRF(MiddlewareMixin):
+class DISABLE_CSRF(MiddlewareMixin):
     def process_request(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
