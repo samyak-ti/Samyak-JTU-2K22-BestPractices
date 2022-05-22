81390a01f79b7c37826235cc186c478971c7f628
samyak-ti <samyak.samdariya@trilogy.com>
2022-05-22 10:21:03 +0530

    Removed Magic Values

---
+++ constants.py
@@ -0,0 +1,9 @@
+# This file will contains all the constants used in the project
+
+# constants for models.py file
+category_max_length = 200
+group_max_length = 100
+expenses_max_length = 200
+expenses_max_digit = 10
+user_expenses_max_digit = 10
+user_expenses_decimal_places = 2
